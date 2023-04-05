9fa65b9457a9e0bf574e7080257a654ca23af4d8
MarcellGranat <granatcellmar98@gmail.com>
2023-04-05 15:12:21 +0200

    final results

--- 00-utils.R
+++ 00-utils.R
@@ -1,5 +1,5 @@
 if (!require(pacman, quietly = TRUE)) install.packages("pacman"); library(pacman)
-p_load("magrittr", "tidyverse", "pins", "currr", "tidymodels", "bonsai")
+p_load("magrittr", "tidyverse", "pins", "currr", "tidymodels", "bonsai", "patchwork")
 p_load_gh("marcellgranat/granatlib")
 p_load_gh("marcellgranat/ggProfessional")
 
